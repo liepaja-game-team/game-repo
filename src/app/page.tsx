3600ab4eb11d67440fd425364bd9d23d4e865998
do0dleman <97869897+do0dleman@users.noreply.github.com>
2024-03-13 12:48:13 +0200

    added session router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { useState } from "react";
 import { api } from "~/trpc/react"
 
 function Home() {
-    const { data: session, isLoading } = api.post.testDb.useQuery({ sessionId: 1 })
+    const { data: session, isLoading, refetch } = api.session.getById.useQuery({ sessionId: 1 })
+    const updateUsername = api.session.addUserName.useMutation()
+    const [userName, setUserName] = useState('')
 
     if (isLoading) {
         return <div>Loading...</div>
@@ -17,6 +20,19 @@ function Home() {
             <div>
                 <p>ID: {session.id}</p>
                 <p>UserName: {session.userName}</p>
+            </div>
+            <div>
+                <input type="text" value={userName}
+                    className="bg-transparent outline-none border focus:border-2"
+                    onChange={(e) => {
+                        setUserName(e.target.value)
+                    }} />
+                <button onClick={async () => {
+                    await updateUsername.mutateAsync({ id: 1, userName: userName })
+                    refetch()
+                }}>
+                    Add Username
+                </button>
             </div>
         </div>
     )
---
+++ src/server/api/routers/session.ts
@@ -0,0 +1,40 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+
+export const sessionRouter = createTRPCRouter({
+    create: publicProcedure
+        .mutation(async ({ ctx }) => {
+            const newSession = await ctx.db.session.create({})
+            return newSession.id
+        }),
+    addUserName: publicProcedure
+        .input(z.object({
+            id: z.number(),
+            userName: z.string()
+                .min(2, 'Username must be at least 2 characters long')
+                .max(32, "Username can not contain more than 32 characters")
+        }))
+        .mutation(async ({ ctx, input }) => {
+            await ctx.db.session.update({
+                data: {
+                    userName: input.userName
+                },
+                where: {
+                    id: input.id
+                }
+            })
+        }),
+    getById: publicProcedure
+        .input(z.object({ sessionId: z.number() }))
+        .query(async ({ input, ctx }) => {
+            const testSession = await ctx.db.session.findFirst({
+                where: {
+                    id: input.sessionId
+                }
+            })
+            return testSession
+        }),
+
+});
