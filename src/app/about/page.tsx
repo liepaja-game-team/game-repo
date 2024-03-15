63bb9d6ac55fdf2cf3e0046f7e4f4f9d8bf91c66
do0dleman <97869897+do0dleman@users.noreply.github.com>
2024-03-15 19:56:56 +0200

    updated about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,8 +6,10 @@ function AboutPage() {
         <div className="flex flex-col">
             <h1 className="text-h1 text-4xl mb-4">Noteikumi</h1>
             <Ul>
-                <li>Jums ir jāatbild uz 4 jautājumiem</li>
-                <li>Uz katru jautājumu ir 1 pareiza atbilde</li>
+                <li>Jums ir jāizvēlas 1 no 4 torta gabaliņiem</li>
+                <li>Katram gabaliņam ir viens jautājums</li>
+                <li>Uz katru jautājumu ir tikai 1 pareiza atbilde</li>
+                <li>Punktu skaits ir atkarīgs no atbildes ātruma.</li>
                 <li>Jauku spēlēšanos!</li>
             </Ul>
             <MainButton href="/game" >
